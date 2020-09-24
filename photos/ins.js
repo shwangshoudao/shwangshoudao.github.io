930b13736c02da04ae7e73769404ae04881438d8
shwangshoudao <shwangshoudao@163.com>
2020-09-24 12:17:33 +0800

    Site updated: 2020-09-24 12:17:31

--- photos/ins.js
+++ photos/ins.js
@@ -120,11 +120,11 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/shwangshoudao/blogpic/master/mini_photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/shwangshoudao/blogpic/master/mini_photos/' + data.link[i]+'.min.jpg';
           var src = 'https://raw.githubusercontent.com/shwangshoudao/blogpic/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '';
+          //src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
